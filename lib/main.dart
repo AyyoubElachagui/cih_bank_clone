29abf44ca08c0ad0629d0ec1855b31acee483f94
AyyoubElachagui <best_way_development@outlook.fr>
2021-08-03 20:03:23 +0200

    CIH BANK Clone Android & Ios App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:clone_cih_bank/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +15,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: Home(),
+      home: SplashScreen(),
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder : (_) => Home()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Center(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Image.asset('assets/images/cih.png'),
+                Text(
+                    'La banque de demain dès aujourd\'hui '
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
